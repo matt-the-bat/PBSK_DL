3dfa18784e8d88558a2961601d92f4cb1c687323
matt-the-bat <matthew.r.blissett@gmail.com>
2024-02-20 09:43:22 -0600

    Revert "Arguments, and using requests b/c stdlib" Forgot to commit a change in between writing
    This reverts commit 4f6809daef2704d37a189fe2b7a84f719e7fe127.

--- PBSK_DL.py
+++ PBSK_DL.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
-import argparse
+import sys
+import urllib.request
 import json
 from pathlib import Path
-from typing import Dict, List, Tuple
-
-import requests
-from pycaption import (CaptionConverter, DFXPReader, SAMIReader, SRTWriter,
-                       WebVTTReader, detect_format)
-from rich.progress import track
+from pycaption import (
+    CaptionConverter,
+    detect_format,
+    SAMIReader,
+    DFXPReader,
+    WebVTTReader,
+    SRTWriter,
+)
+from typing import List, Dict, Tuple
 
 output_root = Path().cwd()
-url_root = "https://content.services.pbskids.org/v2/kidspbsorg/"
 
 
 def mapchars(x: str) -> str:
@@ -82,8 +85,7 @@ def subDownload(cc: Tuple[str, str, str], out_title: Path):
     out_title is name of file, minus suffix"""
     try:
         sub_Path = out_title.with_suffix("." + cc[1])
-        # urllib.request.urlretrieve(cc[0], str(sub_Path))
-        response = requests.get(cc[0]).content
+        urllib.request.urlretrieve(cc[0], str(sub_Path))
         if cc[1] != "srt":
             """Convert webvtt to srt, because
             Kodi 19 will crash on presence of a webvtt file
@@ -117,28 +119,7 @@ def jdownload(jcontent: Dict):
             mp4 = item["mp4"]  # URL
             # Prevent re-downloading existing mp4
             if not out_mp4.is_file():
-                response = requests.get(mp4, f"{out_mp4}", stream=True)
-
-                total_size = int(response.headers.get("content-length", 0))
-                block_size = 1024
-                chunks = response.iter_content(chunk_size=block_size)
-
-                with open(out_mp4, "wb") as file:
-                    with track(
-                        total=total_size,
-                        transient=True,
-                        description="Downloading...",
-                    ) as progress_bar:
-                        for chunk in track(
-                            chunks
-                        ):  # Track the chunks as the sequence
-                            if chunk:  # Filter out keep-alive new chunks
-                                file.write(chunk)
-                                progress_bar.update(len(chunk))
-
-                if total_size != 0 and progress_bar.completed != total_size:
-                    raise RuntimeError("Could not download file")
-
+                urllib.request.urlretrieve(mp4, f"{out_mp4}")
         except Exception:
             print("No valid mp4!")
             raise
@@ -148,32 +129,17 @@ def jdownload(jcontent: Dict):
 
 def main():
     """Examples: 'peg-cat', 'daniel-tigers-neighborhood'"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "show_name",
-        default="daniel-tigers-neighborhood",
-        nargs="?",
-        help="The name of the show",
-    )
+    try:
+        show_name = sys.argv[1]
+    except IndexError:
+        show_name = "daniel-tigers-neighborhood"
 
-    args = parser.parse_args()
+    urlroot = "https://content.services.pbskids.org/v2/kidspbsorg/programs/"
+    contents = urllib.request.urlopen(urlroot + show_name).read()
+    jcontent = json.loads(contents)
+    """ DOWNLOAD VIDEOS """
 
-    if args.show_name:
-        show_name = args.show_name
-        url_Progs = url_root + "programs/"
-        jcontent = requests.get(url_Progs + show_name).json()
-
-        """ DOWNLOAD VIDEOS """
-        """from rich import print
-
-        print(f"{url_root= }\n{show_name= }")
-        print(jcontent)"""
-        jdownload(jcontent)
-
-    elif args.list:
-        """Query list of shows"""
-        url_home = url_root + "home/"
-    # Query
+    jdownload(jcontent)
 
 
 if __name__ == "__main__":
