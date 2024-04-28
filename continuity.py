449975dc54c4b4f582bf5c44f7b206b50f24217c
matt-the-bat <matthew.r.blissett@gmail.com>
2024-04-28 08:33:06 -0500

    Output improvements

--- continuity.py
+++ continuity.py
@@ -12,26 +12,28 @@ workDir = Path().home() / 'external/westley/pbskids' /\
 """
 ffmpeg -v error -i filename.mp4 -vn -c copy -f null - 2>error.log
 """
-for workFile in workDir.iterdir():
+for workFile in sorted(workDir.iterdir()):
     if '.mp4' in workFile.suffixes:
         try:
             input_file = str(workFile)
-
-            process = (
-                ffmpeg.input(input_file)
-                .output('null', f='null')
+            rprint(workFile.name)
+            # Broken audio stream is fast to detect
+            audio_stream = ffmpeg.input(input_file).audio
+            process = \
+            (
+                ffmpeg
+                .output(audio_stream,
+                        'null', f='null',c='copy')
                 .global_args('-loglevel', 'error')
-                .run(capture_stdout=False, capture_stderr=True)
+                .run(capture_stdout=False,
+                     capture_stderr=True)
             )
             stderr_output = process[1]
             line = str(stderr_output).split(r'\r')[-1]
-            print(line)
-            breakpoint()
 
-            match = re.search(r'partial file', line)
+            match_obj = re.search(r'partial file', line)
 
-            if match:
-                print(match)
+            if not match_obj:
                 rprint('[bright_green]Pass[/bright_green]')
             else:
                 rprint('[bright_red]Redownload[/bright_red]')
