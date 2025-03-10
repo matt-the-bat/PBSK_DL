d969d68ec8146c80c84d920e0a8e7dee1847dc7a
matt-the-bat <matthew.r.blissett@gmail.com>
2025-03-10 13:29:37 -0500

    code convention, scoring 10!

--- find_shows.py
+++ find_shows.py
@@ -7,10 +7,10 @@ Created on Mon Mar 10 12:55:48 2025
 import urllib
 from bs4 import BeautifulSoup
 
-urlroot = "https://pbskids.org/videos"
+URLROOT = "https://pbskids.org/videos"
 show_names = []
 
-with urllib.request.urlopen(urlroot) as html:
+with urllib.request.urlopen(URLROOT) as html:
     # suppose 'html' is your HTML content
     soup = BeautifulSoup(html.read(), 'html.parser')
     # find all <li> elements with class "PropertiesNavigationBanner_emblaSlide__7kbWK
@@ -18,7 +18,8 @@ with urllib.request.urlopen(urlroot) as html:
     # loop through each <li> element and find the <a> tag inside it
     for li in lis:
         a_tag = li.find('a')
-        if a_tag: href = a_tag.get('href')
+        if a_tag:
+            href = a_tag.get('href')
         show_names.append(href[8:]) # append the href value
 for _ in sorted(show_names):
     print(_)
