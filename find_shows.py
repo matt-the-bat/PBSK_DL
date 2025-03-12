4c1175c27cd8dc03e83cecfba0bb01c16cd665a8
matt-the-bat <matthew.r.blissett@gmail.com>
2025-03-12 05:51:02 -0500

    Leaving a string of shows found

--- find_shows.py
+++ find_shows.py
@@ -1,28 +1,88 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 10 12:55:48 2025
+
+Shows on Tue Mar 11th 2025:
+    
+almas-way
+arthur
+brambletown
+carl-the-collector
+city-island
+clifford-the-big-red-dog
+curious-george
+cyberchase
+daniel-tigers-neighborhood
+design-squad
+dinosaur-train
+donkey-hodie
+elinor-wonders-why
+hero-elementary
+jamming-on-the-job
+jelly-ben-pogo
+keyshawn-solves-it
+lets-go-luna
+lyla-in-the-loop
+martha-speaks
+maya-miguel
+mecha-builders
+milo
+mister-rogers-neighborhood
+molly-of-denali
+nature-cat
+odd-squad
+oh-noah
+pbs-kids-mega-wow
+pbs-kids-talk-about
+peg-cat
+pinkalicious-and-peterrific
+plum-landing
+ready-jet-go
+rocket-saves-the-day
+rosies-rules
+scigirls
+scribbles-and-ink
+search-it-up
+sesame-street
+sid-the-science-kid
+skillsville
+splash-and-bubbles
+super-why
+super-whys-comic-book-adventures
+team-hamster-ruff-ruffman
+the-cat-in-the-hat-knows-a-lot-about-that
+through-the-woods
+tiny-time-travel
+together-we-can
+what-can-you-become
+wild-kratts
+word-world
+wordgirl
+work-it-out-wombats
+xavier-riddle-and-the-secret-museum
 
 """
 import urllib
 from bs4 import BeautifulSoup
 
+
 def run():
     ''' Open the website to look for videos '''
     URLROOT = "https://pbskids.org/videos"
+    MAGICSTRING = 'PropertiesNavigationBanner_emblaSlide__7kbWK'
     show_names = []
-    
+
     with urllib.request.urlopen(URLROOT) as html:
         # suppose 'html' is your HTML content
         soup = BeautifulSoup(html.read(), 'html.parser')
-        # find all <li> elements with class "PropertiesNavigationBanner_emblaSlide__7kbWK
-        lis = soup.find_all('li', class_='PropertiesNavigationBanner_emblaSlide__7kbWK')
+        # find all <li> elements with class
+        lis = soup.find_all('li', class_=MAGICSTRING)
         # loop through each <li> element and find the <a> tag inside it
         for li in lis:
             a_tag = li.find('a')
             if a_tag:
                 href = a_tag.get('href')
-            show_names.append(href[8:]) # append the href value
+            show_names.append(href[8:])  # append the href value
     for _ in sorted(show_names):
         print(_)
 
